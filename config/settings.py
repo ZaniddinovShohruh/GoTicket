776faaa28769ba03a6ac4b013bcba8fa544295a1
UsmonjonQahhorov <qohhorovusmonjon@gmail.com>
2025-09-13 23:23:37 +0500

    goticket

--- config/settings.py
+++ config/settings.py
@@ -105,12 +105,24 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'goticket',
+        'USER': 'postgres',
+        'PASSWORD': 'Sh998570167',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
 
 AUTH_USER_MODEL =  'goticket.User'
 
